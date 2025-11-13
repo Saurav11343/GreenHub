ebac8eb80a6a9d2a274e8c1b767cc90a5fbc7e99
Saurav Rajput <sauravrajput11343@gmail.com>
2025-11-13 22:47:39 +0530

    signup controller add

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,7 +2,8 @@ import express from "express";
 import dotenv from "dotenv";
 import path from "path";
 
-import authRoutes from "../routes/auth.routes.js";
+import authRoutes from "./routes/auth.routes.js";
+import { connectDB } from "./lib/db.js";
 
 dotenv.config();
 
@@ -11,15 +12,19 @@ const __dirname = path.resolve();
 
 const PORT = process.env.PORT;
 
+app.use(express.json()); //req.body
 
-app.use("/api/auth",authRoutes);
+app.use("/api/auth", authRoutes);
 
-if(process.env.NODE_ENV == "production"){
-    app.use(express.static(path.join(__dirname,"../frontend/dist")));
+if (process.env.NODE_ENV == "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-    app.get("*", (req,res) => {
-        res.sendFile(path.join(__dirname, "../frontend","dist","index.html"));
-    })
-};
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
-app.listen(PORT, () => console.log("Server running on port : " + PORT));
+app.listen(PORT, () => {
+  console.log("Server running on port : " + PORT);
+  connectDB();
+});
