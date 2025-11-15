b597be9b7bdc20a54dc4e7aee164094e505b6af9
Saurav Rajput <sauravrajput11343@gmail.com>
2025-11-15 23:48:27 +0530

    order validation and CRUD operation are done

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,6 +7,7 @@ import authRoutes from "./routes/auth.routes.js";
 import categoryRoutes from "./routes/category.routes.js";
 import plantRoutes from "./routes/plant.routes.js";
 import cartItemRoutes from "./routes/cartItem.routes.js";
+import orderRoutes from "./routes/order.routes.js";
 
 import { connectDB } from "./lib/db.js";
 import { seedRoles } from "./utils/seedRole.js";
@@ -25,6 +26,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/category", categoryRoutes);
 app.use("/api/plant", plantRoutes);
 app.use("/api/cartItem", cartItemRoutes);
+app.use("/api/order", orderRoutes);
 
 if (ENV.NODE_ENV == "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
---
+++ shared/validations/order.validation.js
@@ -0,0 +1,23 @@
+import { z } from "zod";
+
+export const createOrderSchema = z.object({
+  userId: z.string().min(1, "User ID is required"),
+  totalAmount: z.coerce.number().positive("Total amount must be positive"),
+  shippingAddress: z
+    .string()
+    .min(5, "Shipping address  must be at least 5 characters"),
+});
+
+export const updateOrderStatusSchema = z.object({
+  status: z.enum(["Pending", "Shipped", "Delivered", "Cancelled"], {
+    message: "Invalid order status",
+  }),
+});
+
+export const orderIdSchema = z.object({
+  id: z.string().min(1, "Order ID is required"),
+});
+
+export const userIdSchema = z.object({
+  id: z.string().min(1, "User ID is required"),
+});
