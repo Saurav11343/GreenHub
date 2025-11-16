66164eefb19b4a327b90d4b000cc2eb22a2746a3
Saurav Rajput <sauravrajput11343@gmail.com>
2025-11-17 00:52:02 +0530

    minior changes

--- frontend/src/components/layout/NavbarD.jsx
+++ frontend/src/components/layout/NavbarD.jsx
@@ -1,4 +1,8 @@
 import React from "react";
+import { useAuthStore } from "../../store/useAuthStore";
+import DefaultNavbar from "../layout/DefaultNavbar";
+import AdminNavbar from "../layout/AdminNavbar";
+import CustomerNavbar from "../layout/CustomerNavbar";
 
 function NavbarD() {
   const authUser = useAuthStore((state) => state.authUser);
