2da28dfb433383af7c082d1293c85a38a8eec149
Saurav Rajput <sauravrajput11343@gmail.com>
2025-12-13 10:58:44 +0530

    Use Error Fix

--- frontend/src/pages/customer/PlantDetail.jsx
+++ frontend/src/pages/customer/PlantDetail.jsx
@@ -1,4 +1,4 @@
-import React, { use } from "react";
+import React from "react";
 import { useParams, useNavigate } from "react-router";
 import { usePlantStore } from "../../store/usePlantStore";
 import {
