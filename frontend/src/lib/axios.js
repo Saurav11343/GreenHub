ccb1bf6d972ad88af0013e5131f2dd69a60a941c
Saurav Rajput <sauravrajput11343@gmail.com>
2026-03-28 17:12:40 +0530

    added env variable for frontend

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,9 +1,8 @@
 import axios from "axios";
 
+const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:3000/api";
+
 export const axiosInstance = axios.create({
-  baseURL:
-    import.meta.env.MODE === "development"
-      ? "http://localhost:3000/api"
-      : "/api",
+  baseURL: BASE_URL,
   withCredentials: true,
 });
